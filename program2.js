a9eeda48b4b3236e32a0b121f8df7473d08b6894
kumkumsahu1505 <kumkumsahu1505@gmail.com>
2024-09-27 12:08:14 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -1,53 +1,6 @@
 const decodeTheRing = function (s, p) {
+
   
-  function isMatch(message, pattern) {
-    const memo = {};
-  
-    function dfs(mIndex, pIndex) {
-      const key = ${mIndex}:${pIndex};
-      if (key in memo) return memo[key];
-  
-      // If both message and pattern are fully processed
-      if (mIndex >= message.length && pIndex >= pattern.length) {
-        return true;
-      }
-  
-      // If pattern is consumed but message is not
-      if (pIndex >= pattern.length) {
-        return false;
-      }
-  
-      // If current pattern character is '*'
-      if (pattern[pIndex] === '*') {
-        // * can represent zero or more characters
-        if (dfs(mIndex, pIndex + 1) || (mIndex < message.length && dfs(mIndex + 1, pIndex))) {
-          memo[key] = true;
-          return true;
-        }
-      }
-  
-      // If current pattern character is '?'
-      if (pattern[pIndex] === '?' && mIndex < message.length) {
-        if (dfs(mIndex + 1, pIndex + 1)) {
-          memo[key] = true;
-          return true;
-        }
-      }
-  
-      // Check if characters match
-      if (mIndex < message.length && pattern[pIndex] === message[mIndex]) {
-        if (dfs(mIndex + 1, pIndex + 1)) {
-          memo[key] = true;
-          return true;
-        }
-      }
-  
-      memo[key] = false;
-      return false;
-    }
-  
-    return dfs(0, 0);
-  }
   };
   
   module.exports = decodeTheRing;
